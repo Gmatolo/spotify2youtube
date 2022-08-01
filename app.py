06b628559c9f8c9c4e644a3ed7de5fa6851a6c0b
gmatolo <geraldmuli7@gmail.com>
2022-08-01 22:29:59 +0300

    Adds login route

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ from flask import Flask, url_for, session, request, redirect
 
 # App config
 app = Flask(__name__)
+
+@app.route('/')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    print(auth_url)
+    return redirect(auth_url)
