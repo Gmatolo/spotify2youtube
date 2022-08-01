f95672468f8b91df1864ebeed0e5a8283e70670b
gmatolo <geraldmuli7@gmail.com>
2022-08-01 21:28:43 +0300

    adds packages

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'flask',
+    'spotipy',
+    'html5lib',
+    'requests',
+    'requests_html',
+    'beautifulsoup4',
+    'youtube_dl',
+    'pathlib',
+    'pandas'
+]
