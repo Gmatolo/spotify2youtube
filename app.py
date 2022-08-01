a654e1cf29340878ce28c3c496f2540c9ca34d83
gmatolo <geraldmuli7@gmail.com>
2022-08-01 22:33:14 +0300

    Creates the getracks endpoints and stores results to a list

--- app.py
+++ app.py
@@ -27,3 +27,24 @@ def logout():
     for key in list(session.keys()):
         session.pop(key)
     return redirect('/')
+
+
+@app.route('/getTracks')
+def get_all_tracks():
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    results = []
+    iter = 0
+    while True:
+        offset = iter * 50
+        iter += 1
+        curGroup = sp.current_user_saved_tracks(limit=50, offset=offset)['items']
+        for idx, item in enumerate(curGroup):
+            track = item['track']
+            val = track['name'] + " - " + track['artists'][0]['name']
+            results += [val]
+        if (len(curGroup) < 50):
+            break
