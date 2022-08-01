759addcc883d0268bfc49eb05a14f10a2b6e5262
gmatolo <geraldmuli7@gmail.com>
2022-08-01 22:44:29 +0300

    Creates Spotify Oauth

--- app.py
+++ app.py
@@ -74,3 +74,11 @@ def get_token():
 
     token_valid = True
     return token_info, token_valid
+
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+            client_id="id",
+            client_secret="secret",
+            redirect_uri=url_for('authorize', _external=True),
+            scope="user-library-read")
